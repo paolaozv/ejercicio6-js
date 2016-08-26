5599a3f958ad29da699837ace59b7aefc196f7bd
Paola Ortiz de Zevallos <paolaozv@gmail.com>
2016-08-25 19:53:58 -0500

    ejercicio 6

---
+++ app/app.js
@@ -0,0 +1,9 @@
+var numero = prompt("Ingresa un número");
+    numero = parseInt(numero);
+   
+   if (numero % 2 == 0) {
+   		document.write("Número par");
+   }
+   else {
+   		document.write("Número impar");
+   }
